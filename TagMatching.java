d255cc39b97f226bbeac66cbc361874b66ca2d9a
Andreas Sofias <and.sofias@yahoo.gr>
2018-11-05 11:46:52 +0200

    Add files via upload

---
+++ TagMatching.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+
+public class TagMatching {
+
+	public static void main(String[] args) 
+	{
+	  if (args.length!=1)
+	  {
+		System.err.println("Wrong set of arguments.Please input ONLY ONE path to html document");  
+		System.exit(1);
+	  }
+	  else 
+	  {		
+		  StringStackImpl stack = new StringStackImpl();
+		  String filePath = args[0];
+		  
+	  }
+
+}
+}
